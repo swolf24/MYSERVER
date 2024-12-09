ec5f1d43e38376ecdc9cb415ac454f2711be1b75
Sophie Wolf <s.maren.w@gmail.com>
2024-12-09 21:04:41 +0200

    import the rout files

--- server.js
+++ server.js
@@ -3,16 +3,49 @@ import bodyParser from 'body-parser';
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
-var app = express(); 
+import genreRoutes from './routes/genres.js';
+import movieRoutes from './routes/movies.js';
+import userRoutes from './routes/users.js';
+import reviewRoutes from './routes/reviews.js';
+import favoriteRoutes from './routes/favorites.js';
+
+dotenv.config();
+
+const app = express(); // I was reading that it is the saver variant
+//const PORT = process.env.PORT || 3000;
 
 app.listen(3001, () => {
     console.log('The server is running !!');
 });
+app.use(bodyParser.json());
 
-//Let's define an endpoint
+// PostgreSQL connection
+const db = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+db.connect()
+    .then(() => console.log('Connected to PostgreSQL'))
+    .catch((err) => console.error('Database connection error:', err.stack));
+
+    app.use(genreRoutes);
+app.use(movieRoutes);
+app.use(userRoutes);
+app.use(reviewRoutes);
+app.use(favoriteRoutes);
+
+//define an endpoint
 
 app.get('/',(req, res) => {
 
-    //hwere we go if this endpoint is called (e.g. from browser)
+   
         res.send('<h1>You just called root endpoint!!</h1>');
+});
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
